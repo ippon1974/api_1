71f08115e4c3fcb5bb2547e8cd2084efdf414fd5
Vovan Office <plategi@yandex.ru>
2022-09-07 17:11:44 +0300

    office

--- src/main/java/com/example/api_1/controller/AirlineController.java
+++ src/main/java/com/example/api_1/controller/AirlineController.java
@@ -2,10 +2,13 @@ package com.example.api_1.controller;
 
 import com.example.api_1.entity.AirlinesEntity;
 import com.example.api_1.entity.TestEntity;
+import com.example.api_1.exception.AirlinesNotFoundException;
+import com.example.api_1.exception.TestNotFoundException;
 import com.example.api_1.service.AirlineService;
 import com.example.api_1.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +28,19 @@ public class AirlineController {
     @ResponseStatus(HttpStatus.OK)
     public List<AirlinesEntity> getAll(){
         return airlineService.findAllElements();
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity getOneUser(@RequestParam long id){
+        try {
+            return ResponseEntity.ok(airlineService.getOne(id));
+        }catch (AirlinesNotFoundException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (Exception e){
+            return ResponseEntity.badRequest().body("Произщшла ошибка");
+        }
     }
 
 }
--- src/main/java/com/example/api_1/service/AirlineService.java
+++ src/main/java/com/example/api_1/service/AirlineService.java
@@ -1,6 +1,11 @@
 package com.example.api_1.service;
 
 import com.example.api_1.entity.AirlinesEntity;
+import com.example.api_1.entity.TestEntity;
+import com.example.api_1.exception.AirlinesNotFoundException;
+import com.example.api_1.exception.TestNotFoundException;
+import com.example.api_1.model.Airlines;
+import com.example.api_1.model.Test;
 import com.example.api_1.repository.AirlinesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +19,14 @@ public class AirlineService {
 
     public List<AirlinesEntity> findAllElements() {
         return airlinesRepo.findAll();
+    }
+
+    public Airlines getOne(Long id) throws AirlinesNotFoundException {
+        AirlinesEntity air = airlinesRepo.findById(id).get();
+        if(air == null) {
+            throw new AirlinesNotFoundException("Пользователь с таким именем не нейден");
+        }
+        return Airlines.toModel(air);
     }
 
 }
