8bd3e34bdd9f29f17a68aa07d68d319152719c28
Vovan Office <plategi@yandex.ru>
2022-09-08 18:54:13 +0300

    office

--- src/main/java/com/example/api_1/model/Airlines.java
+++ src/main/java/com/example/api_1/model/Airlines.java
@@ -6,9 +6,7 @@ public class Airlines {
     private long airlines_id;
     private String code;
     private String name;
-    private String name_ru;
     private String country;
-    private String country_ru;
 
     public Airlines() {
     }
@@ -37,14 +35,6 @@ public class Airlines {
         this.name = name;
     }
 
-    public String getName_ru() {
-        return name_ru;
-    }
-
-    public void setName_ru(String name_ru) {
-        this.name_ru = name_ru;
-    }
-
     public String getCountry() {
         return country;
     }
@@ -53,22 +43,12 @@ public class Airlines {
         this.country = country;
     }
 
-    public String getCountry_ru() {
-        return country_ru;
-    }
-
-    public void setCountry_ru(String country_ru) {
-        this.country_ru = country_ru;
-    }
-
     public static Airlines toModel(AirlinesEntity airlinesEntity){
         Airlines model = new Airlines();
         model.setAirlines_id(airlinesEntity.getAirlines_id());
         model.setCode(airlinesEntity.getCode());
         model.setName(airlinesEntity.getName());
-        model.setName_ru(airlinesEntity.getName_ru());
         model.setCountry(airlinesEntity.getCountry());
-        model.setCountry_ru(airlinesEntity.getCountry_ru());
         return model;
     }
 }
