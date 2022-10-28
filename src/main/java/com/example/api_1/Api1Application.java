02b3542ad29d7b70ce2ba6a20b246ffa550cc01f
Vovan Office <plategi@yandex.ru>
2022-10-28 11:23:20 +0300

    office

--- src/main/java/com/example/api_1/Api1Application.java
+++ src/main/java/com/example/api_1/Api1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Api1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Api1Application.class, args);
 	}
